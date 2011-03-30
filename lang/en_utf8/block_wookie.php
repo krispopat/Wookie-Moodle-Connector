345eb446c18e5607c8089d92af58ddc3b4a612dd
Michael Hughes <michaelhughes@strath.ac.uk>
2011-03-30 13:29:47 +0100

    Added en_utf8 language pack

---
+++ lang/en_utf8/block_wookie.php
@@ -0,0 +1,2 @@
+<?php
+$string['moodleparamshelp'] ='<p><strong>Note: Some awareness of the Moodle code is required here! and you can <em>seriously</em> break your server</strong></p><p>This setting allows you to specify Moodle variables that will be passed to the Widget.</p><p>Each item should be separated by a comma and takes the form of <em>widgetPreferenceName</em>=<em>moodleVariable</em>. e.g. <code>courseid=$COURSE->id</code> will pass the Moodle <code>$COURSE->id</code> variable to the widget as <code>courseid</code>.';
